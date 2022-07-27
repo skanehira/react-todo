ad9069425b3db5f55f629f49bae14706341ab950
skanehira <sho19921005@gmail.com>
2022-07-28 06:07:52 +0900

    impl

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("register todo", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const inputElm = screen.getByRole<HTMLInputElement>("textbox");
+  userEvent.type(inputElm, "hello");
+  userEvent.click(screen.getByRole("button", { name: "register" }));
+
+  expect(inputElm.value).toBe("");
+
+  userEvent.type(screen.getByRole("textbox"), "world");
+  userEvent.click(screen.getByRole("button", { name: "register" }));
+
+  expect(screen.getAllByRole("listitem")).toHaveLength(2);
+});
+
+test("done todo", () => {
+  render(<App />);
+
+  userEvent.type(screen.getByRole<HTMLInputElement>("textbox"), "hello");
+  userEvent.click(screen.getByRole("button", { name: "register" }));
+
+  userEvent.type(screen.getByRole<HTMLInputElement>("textbox"), "world");
+  userEvent.click(screen.getByRole("button", { name: "register" }));
+
+  const todos = screen.getAllByRole<HTMLLIElement>("listitem");
+  userEvent.click(todos[0]);
+  expect(todos[0]).toHaveStyle("text-decoration:line-through");
+  expect(todos[1]).toHaveStyle("text-decoration:none");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+
+type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+type Todos = Todo[];
+type SetTodos = (todos: Todo[]) => void;
+type SetTodo = (todo: string) => void;
+
+type RegisterProps = {
+  todo: string;
+  todos: Todos;
+  setTodos: SetTodos;
+  setTodo: SetTodo;
+};
+
+type DoneTodoProps = {
+  todos: Todos;
+  setTodos: SetTodos;
+};
+
+type TodoListProps = {
+  todos: Todos;
+  setTodos: SetTodos;
+};
+
+let nextID = 0;
+
+const Input = (props: any) => {
+  return (
+    <input
+      ref={props.inputRef}
+      value={props.todo}
+      onChange={(e) => props.setTodo(e.target.value)}
+    />
+  );
+};
+
+const register = (props: RegisterProps) => {
+  if (!props.todo) {
+    return;
+  }
+  const newTodos = [
+    { id: nextID++, text: props.todo, done: false },
+    ...props.todos,
+  ];
+  props.setTodos(newTodos);
+  props.setTodo("");
+};
+
+const Register = (props: any) => {
+  return (
+    <input
+      style={{ margin: "3px" }}
+      id="input"
+      type="button"
+      value="register"
+      onClick={props.onClick}
+    />
+  );
+};
+
+const doneTodo = (props: DoneTodoProps, todo: Todo) => {
+  const newTodos = props.todos.map((el) => {
+    if (el.id === todo.id) {
+      el.done = !el.done;
+      return el;
+    }
+    return el;
+  });
+  props.setTodos(newTodos);
+};
+
+const TodoList = (props: TodoListProps) => {
+  return (
+    <ul id="todolist">
+      {props.todos.length > 0 &&
+        props.todos.map((todo: Todo) => (
+          <li
+            style={{ textDecoration: todo.done ? "line-through" : "none" }}
+            key={todo.id}
+            onClick={() => doneTodo(props, todo)}
+          >
+            📝 {todo.text}
+          </li>
+        ))}
+    </ul>
+  );
+};
 
 function App() {
+  const [todos, setTodos] = useState([] as Todo[]);
+  const [todo, setTodo] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "10px" }}>
+      <h1>Todo</h1>
+      <Input inputRef={inputRef} todo={todo} setTodo={setTodo} />
+      <Register
+        onClick={() => {
+          register({
+            todos,
+            todo,
+            setTodo,
+            setTodos,
+          });
+          inputRef.current?.focus();
+        }}
+      />
+      <hr />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
